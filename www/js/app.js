0f1422f42bd12c7c02ff471fbff57a341deabb54
hughc4 <hughc4@me.com>
2015-08-20 18:33:33 -0500

    Committing "Hide the status bar"

--- www/js/app.js
+++ www/js/app.js
@@ -16,7 +16,7 @@ app.run(function ($ionicPlatform, $state, $window) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
         if (window.StatusBar) {
-            StatusBar.styleDefault();
+            StatusBar.hide();
         }
     });
     
