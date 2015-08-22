adb342d75f08c1862e9e7c410ab9b52084ec6756
RPHolloway <RPHolloway@users.noreply.github.com>
2015-08-21 20:09:25 -0500

    Reverting back to single view

--- www/js/app.js
+++ www/js/app.js
@@ -3,148 +3,142 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var angular;
-var app = angular.module('starter', ['ionic']);
+var app = angular.module('starter', ['ionic'])
 
-app.run(function ($ionicPlatform, $state, $window) {
-    'use strict';
-    var cordova, StatusBar;
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.overlaysWebView(false);
-            StatusBar.styleDefault();
-        }
-    });
-    
-    function OnScreenSizeChange() {
-        if (window.innerWidth > window.innerHeight)
-            $state.go('landscape');
-        else
-            $state.go('portrait');
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    
-    angular.element($window).bind('resize', OnScreenSizeChange);
-    angular.element($window).bind('load', OnScreenSizeChange);
-    
-    //calling tellAngular on resize event
-    //window.onresize = OnScreenSizeChange;
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+// basicinfo
+// ppeassess
+// ctrlmethod1
+// ctrlmethod2
+// ctrlmethod3
+// ctrlmethod4
+//
+//app.config(function($stateProvider, $urlRouterProvider) {
+//    $stateProvider
+//        .state('basicinfo', {
+//            url: "/basicinfo",
+//            templateUrl: "BasicInfoModel.html",
+//            controller:
+app.config(function($stateProvider, $urlRouterProvider) {
+
+ // Ionic uses AngularUI Router which uses the concept of states
+ // Learn more here: https://github.com/angular-ui/ui-router
+ // Set up the various states which the app can be in.
+ // Each state's controller can be found in controllers.js
+ $stateProvider
+
+ // Each tab has its own nav history stack:
+.state('entry', {
+      url: '/formentry',
+      views: {
+          landscape: {
+              templateUrl: 'landscape.html',
+          },
+          portrait: {
+              templateUrl: 'portrait.html',
+          }
+      }
+   })
+
+ // if none of the above states are matched, use this as the fallback
+ $urlRouterProvider.otherwise('/formentry');
 });
 
-app.config(function ($stateProvider, $urlRouterProvider) {
-
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    'use strict';
-    $stateProvider
-
-        // Each tab has its own nav history stack:
-        .state('landscape', {
-            url: '/landscape',
-            templateUrl: 'landscape.html',
-        })
-         .state('portrait', {
-            url: '/portrait',
-            templateUrl: 'portrait.html',
-        });
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/landscape');
-});
-
-
-app.factory('FormService', function () {
-   'use strict';
-   var service = {}; 
-   var elements = {
-       cb_RespiratorType: 'N/A',
-       cb_GlovesType: 'N/A',
-       cb_Clothing: 'N/A',
-       cb_HearingProtection: 'N/A',
-       cb_ChemClothing: 'N/A',
-       tb_Other: ''
-   };
-   var user = {};
-    
-   
-   
-   service.getelements = function () {
-       return elements;
-   };
-    
-   service.getuser = function () {
-       return user;
-   }
-   
-   return service;
-});
-
-app.controller('ModalCtrl', function ($scope, $ionicModal, $state, $window, FormService) {
-    'use strict';
-    $scope.blank = {};
-    $scope.basicinfo = {};
-    $scope.ppeinfo = {};
-    $scope.user = FormService.getuser();
-    $scope.elements = FormService.getelements();
-    
-    $ionicModal.fromTemplateUrl('./templates/PPEModal.html', {
-        scope: $scope,
-        animation: 'slide-in-up',
-        backdropClickToClose: true
-    }).then(function (modal) {
-        $scope.PPEmodal = modal;
-    });
-    
-    $ionicModal.fromTemplateUrl('./templates/BasicInfoModal.html', {
-        scope: $scope,
-        animation: 'slide-in-up',
-        backdropClickToClose: true
-    }).then(function (modal) {
-        $scope.BasicInfomodal = modal;
-    });
-    
-    $ionicModal.fromTemplateUrl('./templates/Task1Modal.html', {
-        scope: $scope,
-        animation: 'slide-in-up',
-        backdropClickToClose: false
-    }).then(function (modal) {
-        $scope.Task1modal = modal;
-    });
-    $scope.openModal = function (form) {
-        if (form === "PPE") { $scope.PPEmodal.show(); } 
-        else if (form === "BasicInfo") { $scope.BasicInfomodal.show(); }
-        else if (form === "Task1") { $scope.Task1modal.show(); }
+app.directive("breakpoint", function () {
+    return function (scope, element, attrs, ctrl) {
+        var breakpoint = attrs.breakpoint;
+        var mql = window.matchMedia( breakpoint );
+        var mqlHandler = function (mql) {
+            scope.matches = mql.matches;
+            console.log = "Function Called!";
+            if(!scope.$$phase) { //prevents it from unnecessarily calling $scope.$apply when the page first runs
+                scope.$apply();
+            }
+        };
+        mql.addListener(mqlHandler);
+        mqlHandler(mql);
     };
-    $scope.closeModal = function(form) {
-      if (form === "PPE") $scope.PPEmodal.hide();
-      else if (form === "BasicInfo") $scope.BasicInfomodal.hide();
-      else if (form === "Task1") $scope.Task1modal.hide();
-    };
-    //Cleanup the modal when we're done with it!
-    $scope.$on('$destroy', function() {
-      $scope.PPEmodal.remove();
-      $scope.BasicInfomodal.remove();
-      $scope.Task1modal.remove();
-    });
-    // Execute action on hide modal
-    $scope.$on('modal.hidden', function() {
-      // Execute action
-    });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function() {
-      // Execute action
-    });
+});
+
+app.controller('ModalCtrl',function($scope, $ionicModal, $state) {
+  $scope.blank = {}
+  $scope.basicinfo = {}
+  $scope.ppeinfo = {}
+  $scope.user = {}
+  $scope.elements = {
+      cb_RespiratorType: 'N/A',
+      cb_GlovesType: 'N/A',
+      cb_Clothing: 'N/A',
+      cb_HearingProtection: 'N/A',
+      cb_ChemClothing: 'N/A',
+      cm_RespiratorType: false,
+  }
     
-    //--------------Custom Modal Methods-----------------//
-    $scope.submitModal = function(form){
-        if(form == "PPE")
-        {
+  $ionicModal.fromTemplateUrl('./templates/PPEModal.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    backdropClickToClose: true,
+  }).then(function(modal) {
+    $scope.PPEmodal = modal;
+  });
+    
+  $ionicModal.fromTemplateUrl('./templates/BasicInfoModal.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    backdropClickToClose: true,
+  }).then(function(modal) {
+    $scope.BasicInfomodal = modal;
+  });
+    
+  $ionicModal.fromTemplateUrl('./templates/Task1Modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    backdropClickToClose: true,
+  }).then(function(modal) {
+    $scope.Task1modal = modal;
+  });       
+    
+  $scope.openModal = function(form) {
+    if(form == "PPE") $scope.PPEmodal.show();
+    else if(form == "BasicInfo") $scope.BasicInfomodal.show();
+    else if(form == "Task1") $scope.Task1modal.show();
+  };
+  $scope.closeModal = function(form) {
+    if(form == "PPE") $scope.PPEmodal.hide();
+    else if(form == "BasicInfo") $scope.BasicInfomodal.hide();
+    else if(form == "Task1") $scope.Task1modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.PPEmodal.remove();
+    $scope.BasicInfomodal.remove();
+    $scope.Task1modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+    
+  //--------------Custom Modal Methods-----------------//
+  $scope.submitModal = function(form){
+      if(form == "PPE")
+      {
           if($scope.elements.cb_RespiratorType != "N/A") 
               $scope.elements.cm_RespiratorType = true;
           else
@@ -170,32 +164,24 @@ app.controller('ModalCtrl', function ($scope, $ionicModal, $state, $window, Form
           else
               $scope.elements.cm_ChemClothing = false;
           
-          if($scope.elements.tb_Other != "") 
+          if($scope.elements.tb_Other != "N/A") 
               $scope.elements.cm_Other = true;
           else
               $scope.elements.cm_Other = false;
           $scope.ppeinfo = angular.copy($scope.elements);
       }
       
-        //update the master form with the modal elements
-        else if (form == "BasicInfo")
-        {
+      //update the master form with the modal elements
+      else if (form == "BasicInfo")
+      {
           $scope.basicinfo = angular.copy($scope.user);
-        }
-        $scope.closeModal(form);
-    };
+      }
+      $scope.closeModal(form);
+  };
     
-    $scope.reset = function() {
+  $scope.reset = function() {
       $scope.basicinfo = angular.copy($scope.blank);
       $scope.user = angular.copy($scope.blank);
       $scope.ppeinfo = angular.copy($scope.blank);
   };
-    
-    function Refresh()
-    {
-        $scope.ppeinfo = angular.copy($scope.elements);
-        $scope.basicinfo = angular.copy($scope.user);
-        $scope.$apply();
-    }
-    angular.element($window).bind('resize', Refresh);
 });
