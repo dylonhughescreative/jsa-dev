6ec034de2ea7a7da8c42bda99f3eb54a04bc4a6b
RPHolloway <RPHolloway@users.noreply.github.com>
2015-08-27 18:39:53 -0500

    trying to save pdf

--- www/js/pdfBuilder.js
+++ www/js/pdfBuilder.js
@@ -1,9 +1,9 @@
-app.factory('jsPdfBuilder', function($ionicLoading) {
+app.factory('jsPdfBuilder', function ($ionicLoading) {
     'use strict';
-    var instance = {};
-    var doc = {};
-    var pdfOutput = {};
-    var img64 = '';
+    var instance = {},
+        doc = {},
+        pdfOutput = {},
+        img64 = '';
     
     instance.createPdf = function () {
         doc = new jsPDF('landscape', 'mm', 'a4');
@@ -22,10 +22,10 @@ app.factory('jsPdfBuilder', function($ionicLoading) {
     
     function convertImage2Base64(url) {
         var canvas = document.createElement('CANVAS'),
-        ctx = canvas.getContext('2d'),
-        img = new Image;
+            ctx = canvas.getContext('2d'),
+            img = new Image();
         img.crossOrigin = 'Anonymous';
-        img.onload = function() {
+        img.onload = function () {
             var dataUrl;
             canvas.height = img.height;
             canvas.width = img.width;
@@ -34,43 +34,41 @@ app.factory('jsPdfBuilder', function($ionicLoading) {
             img64 = dataUrl;
             canvas = null;
             onLoaded();
-        }
+        };
         img.src = url;
-    };
+    }
     
     function save(filepath) {
-            $ionicLoading.show({
-                template: 'Saving...'
-            });
-            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
-                fs.root.getDirectory( "temp", { create: true },
-                    function(dirEntry) {
-                        dirEntry.getFile( "JSA_Form.pdf", { create: true, exclusive: false }, 
+        $ionicLoading.show({
+            template: 'Saving...'
+        });
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+            fs.root.getDirectory("temp", { create: true },
+                    function (dirEntry) {
+                    dirEntry.getFile("JSA_Form.pdf", { create: true, exclusive: false },
                             function gotFileEntry(fe) {
-                                var fileEntry = fe;
-                                fileEntry.createWriter(function(writer) {
-                                    writer.onwrite = function(evt) {
-                                        console.log("write sucessful");
-                                    };
-                                
-                                    console.log("writing to file");
-                                    writer.write(pdfOutput); 
-                                }, function(error) {
-                                    console.log(error);
-                                });
-                                $ionicLoading.hide();
-                            }, 
-                            function() {
-                                $ionicLoading.hide();
-                                console.log("Get file failed");
-                            }
-                        );
-                    }
-                );
-            },
-            function() {
-                $ionicLoading.hide();
-                console.log("Request for filesystem failed");
-            });
+                            var fileEntry = fe.toURL();
+                            fileTransfer = new FileTransfer();
+                            fileTransfer.download(pdfOutput,
+                                fileEntry,
+                                function (entry) {
+                                    $ionicLoading.hide();
+                                    //$scope.imgFile = entry.toURL();
+                                },
+                                function(error) {
+                                    $ionicLoading.hide();
+                                    alert("Download Error Source -> " + error.source);
+                                },
+                                false,
+                                null
+                                );
+                                                            
+                        });
+                },
+                function () {
+                    $ionicLoading.hide();
+                    console.log("Request for filesystem failed");
+                });
+        });
     }
 });
