da668e04854e619e1bea48c889474dc987313cf3
hughc4 <hughc4@me.com>
2015-08-27 21:31:09 -0500

    Forgot unversioned files apparently.

---
+++ www/js/pdfBuilder.js
@@ -0,0 +1,51 @@
+app.factory('jsPdfBuilder', function ($ionicLoading, $cordovaFile) {
+    var instance = {},
+        doc = {},
+        pdfOutput = '',
+        img64 = '';
+    
+    instance.createPdf = function () {
+        doc = new jsPDF('landscape', 'mm', 'a4');
+        convertImage2Base64('./img/page1.PNG');
+        load();
+    };
+    return instance;
+    
+    function onLoaded() {
+        doc.setFontSize(40);
+        doc.addImage(img64, 'JPEG', 10, 10, 279, 190);
+        doc.text(35, 25, "This is our PDF");
+        //doc.output('dataurlnewwindow');
+        //var pdfOutput = doc.output();
+        //doc.save("JSA_Form.pdf");
+        save("temp/JSA_Form.pdf");
+    }
+    
+    function convertImage2Base64(url) {
+        var canvas = document.createElement('CANVAS'),
+            ctx = canvas.getContext('2d'),
+            img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.onload = function () {
+            var dataUrl;
+            canvas.height = img.height;
+            canvas.width = img.width;
+            ctx.drawImage(img, 0, 0);
+            dataUrl = canvas.toDataURL("image/jpeg", 0.5);
+            img64 = dataUrl;
+            canvas = null;
+            onLoaded();
+        };
+        img.src = url;
+    }
+    
+    function save(filepath) {
+        $cordovaFile.createFile(cordova.file.documentsDirectory, "Rick.pdf", true);
+        $cordovaFile.writeFile(cordova.file.documentsDirectory, "Rick.pdf", "some text", true)
+            .then(function (success) {
+                console.log("works");
+            }, function (error) {
+                console.log("doesnt work");
+            });
+    }
+});
