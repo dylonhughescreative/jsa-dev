20c131860116912329a144e55bfc01ac705dbe47
RPHolloway <RPHolloway@users.noreply.github.com>
2015-08-27 21:43:22 -0500

    removing blob

--- www/js/pdfBuilder.js
+++ www/js/pdfBuilder.js
@@ -13,23 +13,10 @@ app.factory('jsPdfBuilder', function ($ionicLoading, $cordovaFile) {
     
     function onLoaded() {
         doc.setFontSize(40);
-        doc.addImage(img64, 'JPEG', 10, 10, 279, 190);
+        doc.addImage(img64, 'JPEG', 10, 10);
         doc.text(35, 25, "This is our PDF");
         //doc.output('dataurlnewwindow');
         pdfOutput = doc.output();
-        
-        var buffer = new ArrayBuffer(pdfOutput.length);
-        var array = new Uint8Array(buffer);
-        
-        for (var i=0; i<pdfOutput.length; i++)
-        {
-            array[i] = pdfOutput.charCodeAt(i);
-        }
-        
-        blob = new Blob(
-            [array],
-            {type: 'application/pdf', enconding: 'raw'}
-        );
         
         //doc.save("JSA_Form.pdf");
         save("temp/JSA_Form.pdf");
@@ -45,7 +32,7 @@ app.factory('jsPdfBuilder', function ($ionicLoading, $cordovaFile) {
             canvas.height = img.height;
             canvas.width = img.width;
             ctx.drawImage(img, 0, 0);
-            dataUrl = canvas.toDataURL("image/jpeg", 0.5);
+            dataUrl = canvas.toDataURL("image/jpeg", 0.1);
             img64 = dataUrl;
             canvas = null;
             onLoaded();
@@ -55,7 +42,7 @@ app.factory('jsPdfBuilder', function ($ionicLoading, $cordovaFile) {
     
     function save(filepath) {
         $cordovaFile.createFile(cordova.file.documentsDirectory, "JSA_Form.pdf", true);
-        $cordovaFile.writeFile(cordova.file.documentsDirectory, "JSA_Form.pdf", blob, true)
+        $cordovaFile.writeFile(cordova.file.documentsDirectory, "JSA_Form.pdf", pdfOutput, true)
             .then(function (success) {
                 console.log("works");
             }, function (error) {
