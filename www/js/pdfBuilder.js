f17697fbabad338d99a3b037c7af38a00586627a
RPHolloway <RPHolloway@users.noreply.github.com>
2015-08-27 20:51:26 -0500

    removing load

--- www/js/pdfBuilder.js
+++ www/js/pdfBuilder.js
@@ -7,7 +7,6 @@ app.factory('jsPdfBuilder', function ($ionicLoading, $cordovaFile) {
     instance.createPdf = function () {
         doc = new jsPDF('landscape', 'mm', 'a4');
         convertImage2Base64('./img/page1.PNG');
-        load();
     };
     return instance;
     
