40a566896b052dcb09e1e420ff1ad179b0718b7e
hughc4 <hughc4@me.com>
2015-09-03 19:58:00 -0500

    Think that's what we need for side-nav

---
+++ www/PageControl/PageControl.js
@@ -0,0 +1,10 @@
+app.controller('PageCtrl', function($scope) {
+    $scope.wizardSteps = [
+               {step: 'Contractor Type'},
+               {step: 'Basic Information'},
+               {step: 'PPE Assessment'},
+               {step: 'Tasks'},
+               {step: 'Verify'}
+    ];
+               
+});
