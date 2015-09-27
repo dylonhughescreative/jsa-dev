6efc5d55480f24d8ba1089eaf2a56aee05b653c5
RPHolloway <RPHolloway@users.noreply.github.com>
2015-09-26 20:01:29 -0500

    signature and keyboard fix

--- www/FormWizard/Signature.js
+++ www/FormWizard/Signature.js
@@ -1,8 +1,14 @@
-app.controller('SigCtrl', function ($rootScope, $scope, $state, formInfo) {
+app.controller('SigCtrl', function ($rootScope, $scope, $state, $window, formInfo) {
     'use strict';
     
     $scope.signatures = angular.copy(formInfo.getSignatures());
     $scope.commitment = { };
+    
+    $scope.sigCanvas = {
+        scroll: 'false',
+        width: $window.innerWidth - $window.innerWidth*0.02,
+        height: $window.innerHeight * 0.2
+    };
     
     var canvas = document.getElementById('signatureCanvas'),
         signaturePad = new SignaturePad(canvas),
@@ -13,6 +19,7 @@ app.controller('SigCtrl', function ($rootScope, $scope, $state, formInfo) {
         stateController = angular.copy(formInfo.getStateController());
         $scope.commitment = { };
         $scope.clearCanvas();
+        $scope.sigCanvas = $window.width;
     });   
     
     $scope.clearCanvas = function () {
@@ -30,4 +37,11 @@ app.controller('SigCtrl', function ($rootScope, $scope, $state, formInfo) {
         formInfo.setSignatures($scope.signatures);
         $state.go(stateController.nextstate);
     }
+    
+    function refresh() {
+        $scope.sigCanvas.width = $window.innerWidth - $window.innerWidth*0.02;
+        $scope.sigCanvas.height = $window.innerHeight * 0.2;
+        $scope.$apply();
+    }
+    angular.element($window).bind('resize', refresh);
 });
