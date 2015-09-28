8e1b91d0b0aa6944a4b113123ee1462e8964f873
hughc4 <hughc4@me.com>
2015-09-27 19:55:11 -0500

    Adding a signature modal to the commitment page.

--- www/FormWizard/Signature.js
+++ www/FormWizard/Signature.js
@@ -1,8 +1,15 @@
-app.controller('SigCtrl', function ($rootScope, $scope, $state, $window, formInfo) {
+app.controller('SigCtrl', function ($rootScope, $scope, $state, $window, $ionicModal, formInfo) {
     'use strict';
     
     $scope.signatures = angular.copy(formInfo.getSignatures());
     $scope.commitment = { };
+    
+    $ionicModal.fromTemplateUrl('./FormWizard/SignatureModal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.signatureModal = modal;
+    });
     
     $scope.sigCanvas = {
         scroll: 'false',
@@ -10,9 +17,14 @@ app.controller('SigCtrl', function ($rootScope, $scope, $state, $window, formInf
         height: $window.innerHeight * 0.2
     };
     
-    var canvas = document.getElementById('signatureCanvas'),
-        signaturePad = new SignaturePad(canvas),
-        stateController = angular.copy(formInfo.getStateController());
+    var stateController = angular.copy(formInfo.getStateController());
+    var signaturePad;
+    
+    $scope.closeSignatureModal = function() {
+        var sigImg = signaturePad.toDataURL("image/png", 1);
+        $scope.commitment.signature = sigImg;
+        $scope.signatureModal.hide();
+    }
     
     $rootScope.$on('$viewContentLoading', function(event, viewConfig){ 
         $scope.signatures = angular.copy(formInfo.getSignatures());
@@ -24,6 +36,15 @@ app.controller('SigCtrl', function ($rootScope, $scope, $state, $window, formInf
     
     $scope.clearCanvas = function () {
         signaturePad.clear();
+    }
+    
+    $scope.openSignatureModal = function() {
+        $scope.signatureModal.show();
+        var canvas = document.getElementById('signatureCanvas');
+        signaturePad = new SignaturePad(canvas);
+        var ModalDimensions = document.getElementById('SignatureModal');
+        $scope.sigCanvas.width = ModalDimensions.clientWidth - ModalDimensions.clientWidth*0.02;
+        $scope.sigCanvas.height =  ModalDimensions.clientHeight * 0.2;
     }
     
     $scope.saveCanvas = function () {
@@ -43,5 +64,5 @@ app.controller('SigCtrl', function ($rootScope, $scope, $state, $window, formInf
         $scope.sigCanvas.height = $window.innerHeight * 0.2;
         $scope.$apply();
     }
-    angular.element($window).bind('resize', refresh);
+    //angular.element($window).bind('resize', refresh);
 });
