b735c1ce8508f83f704a1fdae953981f511f75db
hughc4 <hughc4@me.com>
2015-10-03 16:06:55 -0500

    Testing keyboard on GCInfo

--- www/FormWizard/GCInfo/GCController.js
+++ www/FormWizard/GCInfo/GCController.js
@@ -6,6 +6,13 @@ app.controller('GCinfoCtrl', function ($rootScope, $scope, formInfo) {
     
     $rootScope.$on('$viewContentLoading', function(event, viewConfig){ 
         $scope.completedElements = angular.copy(formInfo.getcompletedElements());
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.hide();
+        }
     });
     
     function verify() {
