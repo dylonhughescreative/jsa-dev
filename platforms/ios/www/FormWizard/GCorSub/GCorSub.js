40f7ca18fd48c05eed2135e7c258ba1101859310
hughc4 <hughc4@me.com>
2015-10-03 15:26:17 -0500

    Removing all the extra crap I added earlier and adding the splash images to the ios res folder.

--- platforms/ios/www/FormWizard/GCorSub/GCorSub.js
+++
@@ -1,9 +0,0 @@
-app.controller('GCorSub', function ($scope, formInfo) {
-    'use strict';
-    $scope.onclick = function (state) {
-        var BasicInfo = formInfo.getBasicInfo();
-        BasicInfo.gcorsub = state;
-        formInfo.setBasicInfo(BasicInfo);
-        $scope.next(state);
-    };
-});
