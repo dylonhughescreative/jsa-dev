33a50f98ba0e9b911f742440e71830b22603994a
hughc4 <hughc4@me.com>
2015-10-03 16:49:01 -0500

    Fixed the icon and added a navigation sidebar.

--- www/FormWizard/FormWizardController.js
+++ www/FormWizard/FormWizardController.js
@@ -1,10 +1,43 @@
-app.controller('FormWizardCtrl', function($scope) {
+app.controller('FormWizardCtrl', function($scope, $state, $ionicSideMenuDelegate) {
+    'use strict';
     $scope.wizardSteps = [
-               {title: 'Contractor Type'},
-               {title: 'Basic Information'},
-               {title: 'PPE Assessment'},
-               {title: 'Tasks'},
-               {title: 'Verify'}
+               {
+                   title: 'General Information',
+                   link: 'formWizard.GCorSub'
+               },
+               {
+                   title: 'PPE Assessment',
+                   link: 'formWizard.PPE'
+               },
+               {
+                   title: 'Job Elements',
+                   link: 'formWizard.JobElements'
+               },
+               {
+                   title: 'Training Requirements',
+                   link: 'formWizard.TrainReq'
+               },
+               {
+                   title: 'License Requirements',
+                   link: 'formWizard.LicReq'
+               },
+               {
+                   title: 'Area Concerns',
+                   link: 'formWizard.AreaConcerns'
+               },
+               {
+                   title: 'Additional Training',
+                   link: 'formWizard.AddTraining'
+               },
+               {
+                   title: 'Overview',
+                   link: 'overview'
+               },
     ];
+    $scope.GoToPage = function (state)
+    {
+        $state.go(state);
+        $ionicSideMenuDelegate.toggleLeft(false);
+    }
                
 });
