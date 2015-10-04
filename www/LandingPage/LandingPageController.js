4a1e3f3a376e9f6bc3d5ba26fb0ac1bd3803830e
hughc4 <hughc4@me.com>
2015-10-04 02:51:33 -0500

    Trying a way to hide the splash screen.

--- www/LandingPage/LandingPageController.js
+++ www/LandingPage/LandingPageController.js
@@ -1,8 +1,15 @@
-app.controller('HomeCtrl', function ($scope, $state, $ionicSideMenuDelegate) {
+app.controller('HomeCtrl', function ($scope, $state, $ionicSideMenuDelegate, $ionicView) {
     'use strict';
     $scope.GoTo = function (state) {
         $state.go(state);
     };
+    
+    $scope.$on('$ionicView.loaded', function() {
+        ionic.Platform.ready( function() {
+            if (navigator && navigator.splashscreen)
+                navigator.splashscreen.hide();
+        });
+    });
     
     $scope.helpShow = false;
     
