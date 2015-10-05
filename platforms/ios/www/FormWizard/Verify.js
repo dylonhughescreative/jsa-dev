5cec11f08d51e140b2f8babf15bd024b69ab4392
hughc4 <hughc4@me.com>
2015-10-05 18:05:23 -0500

    Committing some changes to the signing flow.

--- platforms/ios/www/FormWizard/Verify.js
+++ platforms/ios/www/FormWizard/Verify.js
@@ -1,4 +1,4 @@
-app.controller('VerifyCtrl', function ($rootScope, $scope, $state, formInfo, jsPdfBuilder) {
+app.controller('VerifyCtrl', function ($rootScope, $scope, $state, $ionicModal, $window, formInfo, jsPdfBuilder) {
     'use strict';
     
     $scope.basicinfo = angular.copy(formInfo.getBasicInfo());
@@ -11,6 +11,107 @@ app.controller('VerifyCtrl', function ($rootScope, $scope, $state, formInfo, jsP
     $scope.signatures = angular.copy(formInfo.getSignatures());
     
     var stateController = { };
+    
+    $scope.signatures = angular.copy(formInfo.getSignatures());
+    $scope.commitment = { };
+    $scope.emptySig = true;
+    
+    $ionicModal.fromTemplateUrl('./FormWizard/SignatureModal.html', {
+        id: "1",
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.signatureModal = modal;
+    });
+    
+    $ionicModal.fromTemplateUrl('./FormWizard/CertificationAwarenessModal.html', {
+        id: "2",
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.certificationModal = modal;
+    });
+    
+    $scope.sigCanvas = {
+        scroll: 'false',
+        width: $window.innerWidth - $window.innerWidth*0.02,
+        height: $window.innerHeight * 0.2
+    };
+    
+    var stateController = angular.copy(formInfo.getStateController());
+    var signaturePad;
+    
+    $scope.closeSignatureModal = function() {
+        $scope.signatureModal.hide();
+        $scope.saveCanvas();
+    }
+    
+    $rootScope.$on('$viewContentLoading', function(event, viewConfig){ 
+        $scope.signatures = angular.copy(formInfo.getSignatures());
+        stateController = angular.copy(formInfo.getStateController());
+        $scope.commitment = { };
+    });   
+    
+    $scope.clearCanvas = function () {
+        signaturePad.clear();
+    }
+    
+    $scope.openSignatureModal = function() {
+        $scope.closeCertModal();
+        if (window.cordova) { 
+            var so = cordova.plugins.screenorientation;
+            so.setOrientation('landscape');
+        }
+        $scope.signatureModal.show();
+        var canvas = document.getElementById('signatureCanvas');
+        var options = {
+            onEnd: function(event) {
+                if (this.isEmpty())
+                    $scope.emptySig = true;
+                else
+                    $scope.emptySig = false;
+            }
+        }
+        signaturePad = new SignaturePad(canvas, options);
+        signaturePad.clear();
+        var ModalDimensions = document.getElementById('SignatureModal');
+        $scope.sigCanvas.width = ModalDimensions.offsetWidth - ModalDimensions.offsetWidth*0.02;
+        $scope.sigCanvas.height =  ModalDimensions.offsetHeight * 0.5;
+    }
+    
+    $scope.saveCanvas = function () {
+        var sigImg = signaturePad.toDataURL("image/png", 1);
+        $scope.commitment.signature = sigImg;
+        $scope.signatures.push({
+            name: $scope.commitment.name,
+            date: $scope.commitment.date,
+            signature: $scope.commitment.signature    
+        });
+        formInfo.setSignatures($scope.signatures);
+        if (window.cordova) {
+            var so = cordova.plugins.screenorientation;
+            so.setOrientation('landscape');
+        }
+        $state.go('completedForm');
+    }
+    
+    $scope.back = function () {
+        if (window.cordova) {
+            var so = cordova.plugins.screenorientation;
+            so.setOrientation('landscape');
+        }
+        $state.go('verify');
+    };
+    
+    function refresh() {
+        var ModalDimensions = document.getElementById('SignatureModal');
+        if (ModalDimensions != null) {
+            $scope.sigCanvas.width = ModalDimensions.offsetWidth - ModalDimensions.offsetWidth*0.02;
+            $scope.sigCanvas.height =  ModalDimensions.offsetHeight * 0.5;
+            $scope.$apply();
+        }
+    }
+    angular.element($window).bind('resize', refresh);
      
     $rootScope.$on('$viewContentLoading', function(event, viewConfig){
         $scope.basicinfo = angular.copy(formInfo.getBasicInfo());
@@ -37,6 +138,23 @@ app.controller('VerifyCtrl', function ($rootScope, $scope, $state, formInfo, jsP
         formInfo.setStateController(stateController);
         $state.go(state);
     };
+    
+    $scope.openCertModal = function() {
+        $scope.certificationModal.show();
+    }
+    
+    $scope.closeCertModal = function() {
+        $scope.certificationModal.hide();
+    }
+    
+    $scope.closeCertModalAndBack = function() {
+        $scope.certificationModal.hide();
+        if (window.cordova) {
+            var so = cordova.plugins.screenorientation;
+            so.setOrientation('landscape');
+        }
+        $state.go('verify');
+    }
     
     $scope.uploadFile = function () {
         jsPdfBuilder.createPdf();
