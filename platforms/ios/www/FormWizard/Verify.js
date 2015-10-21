db59e6f560109d4e0585049f1a6ea484717f4228
RPHolloway <RPHolloway@users.noreply.github.com>
2015-10-20 20:19:06 -0400

    trying with .pdf

--- platforms/ios/www/FormWizard/Verify.js
+++ platforms/ios/www/FormWizard/Verify.js
@@ -347,6 +347,7 @@ app.controller('VerifyCtrl', function ($rootScope, $scope, $state, $ionicModal, 
     }
     
     function upload () {
+        filename += ".pdf";
         jsPdfBuilder.save( filename, function() {
         
             var options = new FileUploadOptions();
